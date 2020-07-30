01ce75a84b6e309dab6bb825f1e80a78e996b439
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2020-07-30 07:15:31 -0600

    Create runprogram.sh

---
+++ runprogram.sh
@@ -0,0 +1,16 @@
+Get to Gitlab Repo
+$ git clone (copy link)
+$ ls
+$ cd (enter desired location)
+$ cd .. (go back one leve)
+
+Run Program
+$ ./(program name) YYYY-MM-DDT00:00:00 YYYY-MM-DDT23:59:59
+
+Remove old files:
+$ rm (file name)
+
+Add files
+$ git add(file)
+$ git commit
+$ git push
