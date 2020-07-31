e18dc2fffb637059a197b4b029b80bcdb66d28f4
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2020-07-31 07:05:44 -0600

    Add files via upload

--- bash basics.sh
+++ bash basics.sh
@@ -1,2 +1,5 @@
-$ module avil (module)
+$ module avil (module) 'Show which modules are avaliable'
 $ groups 'Identify which user groups assgined'
+$ cd 'Change Directory'
+$ ls 'List Files'
+$ ls -l 'List Files with details'
