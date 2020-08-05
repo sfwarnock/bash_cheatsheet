859e07267541562f8aab827b4a9dbbe63c7ee64a
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2020-08-05 14:59:59 -0600

    Add files via upload

--- bash basics.sh
+++ bash basics.sh
@@ -7,3 +7,9 @@ $ ssh (platform name)
 $ pwd 'print working directory'
 $ df -h 'Check disk space'
 $ df -h/tmp 'Check on specific file system'
+$ df -h /ram/tmp
+$ lscpu 'arch look'
+$ free -h
+$ cat /proc/meminfo | head -1
+$ head -1 /proc/meminfo
+$ rpm -qi basesystem
