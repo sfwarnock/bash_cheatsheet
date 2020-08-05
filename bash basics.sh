cb561b13541a295b893f6cc2132d547d55dbac55
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2020-08-05 07:17:34 -0600

    Add files via upload

--- bash basics.sh
+++ bash basics.sh
@@ -4,3 +4,6 @@ $ cd 'Change Directory'
 $ ls 'List Files'
 $ ls -l 'List Files with details'
 $ ssh (platform name)
+$ pwd 'print working directory'
+$ df -h 'Check disk space'
+$ df -h/tmp 'Check on specific file system'
