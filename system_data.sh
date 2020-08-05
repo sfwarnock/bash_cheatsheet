e310bd442862691a7aba6568474375d5a3eb984e
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2020-08-05 15:02:06 -0600

    Add files via upload

---
+++ system_data.sh
@@ -0,0 +1,9 @@
+$ ssh (node)
+
+$ lscpu
+$ free -h
+$ df -h
+$ rpm -qi basesystem
+$ numactl --hardware (hyperthread check)
+
+$ hostname ; uname -a ; lscpu ; numactl --hardware ; free -h ; df -h ; rpm -qi basesystem
